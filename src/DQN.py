4553bf35d22654e615e2bb847b514ddbcbd7e5e6
Totti00 <gek5800@gmail.com>
2023-12-20 16:18:46 +0100

    traslate this file in italian

--- src/DQN.py
+++ src/DQN.py
@@ -20,7 +20,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter()
 
-
+#viene letto un file di configurazione YAML per ottenere le impostazioni dell'ambiente
+#viene caricato il file di waypoints 
 with open('src/map/example_map/config_example_map.yaml') as file:
     conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
@@ -52,7 +53,7 @@ class ReplayMemory(object):
         """Save a transition"""
         self.memory.append(Transition(*args))
 
-    def sample(self, batch_size):
+    def sample(self, batch_size): #ritorna un campione casuale di transizioni di dimensione batch_size
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
@@ -73,8 +74,6 @@ class DQN(nn.Module):
 
 
 
-
-
 BATCH_SIZE = 10
 GAMMA = 0.99
 EPS_START = 0.9
@@ -89,19 +88,15 @@ action_space = gym.spaces.Discrete(13)
 n_actions = action_space.n
 n_observations = 1080
 
+#vengono inizializzate le reti neurali (polocy e taget), l'ottimizzatore e la memoria di riproduzione
 policy_net = DQN(n_observations, n_actions).to(device)
-
 try:
     policy_net.load_state_dict(torch.load("policy_net.pth"))
     policy_net.eval()
 except:
     print("no policy_net.pth file found")
-
-
 target_net = DQN(n_observations, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
-
-
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
 memory = ReplayMemory(100000)
 steps_done = 0
@@ -121,22 +116,19 @@ def select_action(state):
     # else:
     #     return torch.tensor([[action_space.sample()]], device=device, dtype=torch.long)
 
-    
-    
-
-
-
 def optimize_model():
+    #Se la memoria non è abbastanza grande, non viene eseguito l'aggiornamento della rete
     if len(memory) < BATCH_SIZE:
         return
-    transitions = memory.sample(BATCH_SIZE)
+    
     # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
     # detailed explanation). This converts batch-array of Transitions
     # to Transition of batch-arrays.
+    transitions = memory.sample(BATCH_SIZE)
     batch = Transition(*zip(*transitions))
 
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
+    #Vengono preparti i tensori necessari per il calcolo del loss, la mschera per gli stati finali
+    #e i tensori per gli stati, azioni e ricompense.
     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                           batch.next_state)), device=device, dtype=torch.bool)
     non_final_next_states = torch.cat([s for s in batch.next_state
@@ -145,32 +137,34 @@ def optimize_model():
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
+    #Calcola Q(s_t, a) - il modello calcola Q(s_t), quindi selezioniamo le colonne delle azioni intraprese. 
+    #Queste sono le azioni che sarebbero state intraprese per ogni stato batch secondo policy_net
     state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1).values
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
+    #Calcola V(s_{t+1}) per tutti gli stati successivi. I valori previsti delle azioni per 
+    #non_final_next_states vengono calcolati in base al "vecchio" target_net; selezionando la loro 
+    #migliore ricompensa con max(1).values. Questo viene unito in base alla maschera, in modo tale da 
+    #avere il valore dello stato previsto o 0 nel caso in cui lo stato fosse definitivo.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
     with torch.no_grad():
         next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
-    # Compute the expected Q values
 
+    #Calcola il valori Q previsti
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
-    # Compute Huber loss
+    #Calcola il loss utilizzando la Huber Loss tra i valori Q predetti e i valori target
     criterion = nn.SmoothL1Loss()
     loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
     writer.add_scalar("Loss/train", loss, i_episode)
-    # Optimize the model
+
+    # Ottimizza il modello
     optimizer.zero_grad()
+
+    #Viene eseguita la retropropagazione
     loss.backward()
-    # In-place gradient clipping
+    #Si esegue un operazione di clipping dei gradienti per evitare problemi di esplosione del gradiente
     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    #L'ottimizzatore esegue un passo di aggiornamento dei pesi del modello
     optimizer.step()
     torch.save(policy_net.state_dict(), "policy_net.pth")
 
@@ -206,17 +200,16 @@ for i_episode in range(num_episodes):
     state = torch.tensor(state["scans"][0], dtype=torch.float32, device=device).unsqueeze(0)
 
     for t in count():
+        # Viene selezionata un'azione utilizzando la rete neurale dell'agente
         action =  select_action(state)
 
-        #map action 0 to n_actions to -1 to 1
+        #l'azione selezionata viene mappata da un indice discreto a un valore compreso tra -1 e 1
         mapped_action = (action.item() - (n_actions-1)/2)/((n_actions-1))
 
-
-
-
+        #Viene eseguita l'azione nell'ambiente e ottenute le nuove osservazioni, stato, reward e flag di terminazione
         observation, reward, done, info  = env.step(np.array([[mapped_action,3]]))
 
- 
+        #se il valore minimo delle scansioni è minore di 0.5, il reward è 0.001
         if min(observation["scans"][0]) < 0.5:
             reward = 0.001
 
@@ -230,23 +223,24 @@ for i_episode in range(num_episodes):
             next_state = None
             reward = 0
         else:
+            #se non è terminato, viene creato il nuovo stato basato sulle osservazioni ottenute dall'ambiente
             next_state = torch.tensor(observation["scans"][0], dtype=torch.float32, device=device).unsqueeze(0)
         
+        #La ricompensa totale accumulata
         total_reward += reward
         
-
         reward = torch.tensor([reward], device=device)
 
-        # Store the transition in memory
+        # La transizione viene memorizzata nella memoria
         memory.push(state, action, next_state, reward)
 
-        # Move to the next state
+        # Il modello passa al nuovo stato per continuare l'esplorazione e l'apprendimento
         state = next_state
 
-        # Perform one step of the optimization (on the policy network)
+        # Esegue l'ottimizzazione della rete neurale dell'agente
         optimize_model()
 
-        # Soft update of the target network's weights
+        # Viene eseguito un aggiornamente "soft" della rete target, combinando i pesi della rete target con quelli della rete principale
         # θ′ ← τ θ + (1 −τ )θ′
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
@@ -254,9 +248,11 @@ for i_episode in range(num_episodes):
             target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
         target_net.load_state_dict(target_net_state_dict)
 
+        #rendering dell'ambiente
         env.render(mode='human')
 
         if done:
+            #salva su tensorboard
             writer.add_scalar("Reward",total_reward, i_episode)
             writer.add_scalar("time alive", t*timestep, i_episode)
             break
