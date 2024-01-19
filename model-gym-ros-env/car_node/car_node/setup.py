12ae4c3358f6a7bb4d2c03a80cc0c2ca990618a4
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-01-19 23:40:42 +0100

    feat: remove useless files and add Dockerfile to build ros enviroments

--- model-gym-ros-env/car_node/car_node/setup.py
+++ model-gym-ros-env/car_node/car_node/setup.py
@@ -7,7 +7,7 @@ setup(name='f110_gym',
       url='https://f1tenth.org',
       package_dir={'': 'gym'},
       install_requires=['gym==0.19.0',
-		        'numpy<=1.22.0,>=1.18.0',
+		            'numpy==1.23.0',
                         'Pillow>=9.0.1',
                         'scipy>=1.7.3',
                         'numba>=0.55.2',
@@ -15,8 +15,6 @@ setup(name='f110_gym',
                         'pyglet==1.5.28',
                         'pyopengl',
                         # our deps
-                        'stable-baselines3==2.2.1',
-                        'stable-baselines3[extra]',
-                        'Shimmy==1.3.0',
-                        'tensorboard==2.14.0']
+                        'stable-baselines3',
+                        'shimmy>=0.2.1']
       )
