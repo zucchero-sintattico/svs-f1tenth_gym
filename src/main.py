953639ae6e08ac2210daf04471d9ba35ab8e16c5
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-01-25 17:14:20 +0100

    wip: init refactor add argument parser

---
+++ src/main.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+
+from train import train
+
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+map_path = os.path.join(base_path, "map", "f1tenth_racetracks")
+
+def get_available_maps():
+    dir_content = os.listdir(map_path)
+    maps = []
+    for item in dir_content:
+        if os.path.isdir(os.path.join(map_path, item)):
+            maps.append(item)
+    return maps
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="""
+        This is the entry point for the application.
+        You can train the model for autonomous driving or run the model in the simulator.""")
+    parser.add_argument('-l', '--list', action='store_true', help='List all available maps')
+    subparser = parser.add_subparsers(dest="command", help='Select the command to run')
+
+    train_parser = subparser.add_parser('train', help='Train the model')
+    train_parser.add_argument('-m', '--map', type=str, help='Map to train the model on. Default: random maps', default='random')
+    train_parser.add_argument('--timesteps', type=float, help='Number of timesteps to train the model. Default: 0.01', default=0.01)
+    train_parser.add_argument('--min-timesteps', type=int, help='Maximum number of timesteps to train the model. Default: 10_000', default=10_000)
+    train_parser.add_argument('--max-timesteps', type=int, help='Maximum number of timesteps to train the model. Default: 11_000', default=11_000)
+    train_parser.add_argument('--min-learning-rate', type=float, help='Learning rate for the model. Default: 0.0005', default=0.0005)
+    train_parser.add_argument('--max-learning-rate', type=float, help='Learning rate for the model. Default: 0.0001', default=0.0001)
+    train_parser.add_argument('--num-of-steps', type=int, help='Number of steps to train the model. Default: 10', default=10)
+
+    run_parser = subparser.add_parser('run', help='Run the model in the simulator')
+    run_parser.add_argument('-m', '--map', type=str, help='Map to train the model on. Default: random maps', default='random')
+
+    args = parser.parse_args()
+
+    if args.list:
+        print("Available maps:")
+        print(get_available_maps())
+        exit(0)
+    elif args.command == 'train':
+        print("Training the model")
+        train(  args.map == 'random',
+                args.map,
+                args.timesteps,
+                args.min_timesteps,
+                args.max_timesteps,
+                args.min_learning_rate,
+                args.max_learning_rate,
+                args.num_of_steps)
+        exit(0)
+    elif args.command == 'run':
+        print("Running the model")
+        print(args)
+        exit(0)
+    else:
+        print("Invalid command")
+        exit(1)
--- src/train.py
+++ src/train.py
@@ -9,41 +9,33 @@ from utility.map_utility import get_map, get_formatted_map, map_ext
 from utility.linear_schedule import linear_schedule
 from stable_baselines3.common.callbacks import EvalCallback
 
+def train(
+        random_map: bool,
+        map: str,
+        timestep: float,
+        min_timesteps: int,
+        max_timesteps: int,
+        min_learning_rate: float,
+        max_learning_rate: float,
+        num_of_steps: int) -> None:
 
-timestep = 0.01
-tensorboard_path = './train_test/'
+    tensorboard_path = './train_test/'
+    device = 'cpu'
 
+    if random_map:
+        map = "BrandsHatch"
+    path = get_map(map)
+    map_path = get_formatted_map(path)
+    eval_env  = gym.make('f110_gym:f110-v0', map=map_path, map_ext=map_ext, num_agents=1, timestep=timestep, integrator=Integrator.RK4)
+    eval_env = F110_Wrapped(eval_env, random_map=True)
+    eval_env.set_map_path(path)
+    eval_env.seed(1773449316)
 
-map = "BrandsHatch"
-path = get_map(map)
-map_path = get_formatted_map(path)
+    timesteps_list = np.logspace(np.log10(min_timesteps), np.log10(max_timesteps), num=num_of_steps, endpoint=True, base=10.0, dtype=int, axis=0)
+    learning_rate_list = np.logspace(np.log10(max_learning_rate), np.log10(min_learning_rate), num=num_of_steps, endpoint=True, base=10.0, dtype=None, axis=0)
 
-eval_env  = gym.make('f110_gym:f110-v0', map=map_path, map_ext=map_ext, num_agents=1, timestep=timestep, integrator=Integrator.RK4)
-
-eval_env = F110_Wrapped(eval_env, random_map=False)
-
-eval_env.set_map_path(path)
-
-eval_env.seed(1773449316)
-
-skip_training = False
-
-max_timesteps = 11_000
-min_timesteps = 10_000
-
-max_learning_rate = 0.0005
-min_learning_rate = 0.0001
-num_of_steps = 10
-
-device = 'cpu'
-
-timesteps_list = np.logspace(np.log10(min_timesteps), np.log10(max_timesteps), num=num_of_steps, endpoint=True, base=10.0, dtype=int, axis=0)
-learning_rate_list = np.logspace(np.log10(max_learning_rate), np.log10(min_learning_rate), num=num_of_steps, endpoint=True, base=10.0, dtype=None, axis=0)
-
-print("timestemp" , timesteps_list)
-print("leaning rate", learning_rate_list)
-
-if not skip_training:
+    print("timestemp" , timesteps_list)
+    print("leaning rate", learning_rate_list)
 
     for timesteps, learning_rate in zip(timesteps_list, learning_rate_list):
 
@@ -71,22 +63,16 @@ if not skip_training:
                             ) 
                 model.save("./src/base_model")   
 
-
-
         eval_callback = EvalCallback(eval_env, best_model_save_path='./train_test/',
                                     log_path='./train_test/', eval_freq= int(timesteps/20),
                                     deterministic=True, render=False)
-        
-
 
         model.learn(total_timesteps=timesteps, progress_bar=True, callback=eval_callback)
 
         del model 
 
         model = PPO.load("./train_test/best_model", eval_env, device=device)
-
         mean_reward, _ = evaluate_policy(model, model.get_env(), n_eval_episodes=20)
-
 
         #save in file the mean reward, if the file does not exist, create it
         if not os.path.exists("./train_test/mean_reward.txt"):
@@ -103,36 +89,32 @@ if not skip_training:
                 model.save("./train_test/best_global_model")
                 print("Saved Best Model")
 
-        del model 
+        del model
 
 
+# eval_env  = gym.make('f110_gym:f110-v0', map=map_path, map_ext=map_ext, num_agents=1, timestep=timestep, integrator=Integrator.RK4)
+# eval_env = F110_Wrapped(eval_env, random_map=False)
+# eval_env.set_map_path(path)
 
-    
+# model = PPO.load("./train_test/best_global_model", eval_env, device=device)
 
+# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1)
 
-eval_env  = gym.make('f110_gym:f110-v0', map=map_path, map_ext=map_ext, num_agents=1, timestep=timestep, integrator=Integrator.RK4)
-eval_env = F110_Wrapped(eval_env, random_map=False)
-eval_env.set_map_path(path)
+# print(mean_reward)
 
-model = PPO.load("./train_test/best_global_model", eval_env, device=device)
+# # Enjoy trained agent
+# vec_env = model.get_env()
+# obs = vec_env.reset()
 
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1)
+# episode = 0
+# while episode < 100:
 
-print(mean_reward)
-
-# Enjoy trained agent
-vec_env = model.get_env()
-obs = vec_env.reset()
-
-episode = 0
-while episode < 100:
-
-    episode += 1
-    obs = vec_env.reset()
-    done = False
-    while not done:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, rewards, dones, info = vec_env.step(action)
-        if done:
-            print("-->", episode)
-        eval_env.render(mode='human')
+#     episode += 1
+#     obs = vec_env.reset()
+#     done = False
+#     while not done:
+#         action, _states = model.predict(obs, deterministic=True)
+#         obs, rewards, dones, info = vec_env.step(action)
+#         if done:
+#             print("-->", episode)
+#         eval_env.render(mode='human')
